d0ed7175fe46f49f1cd897826c7651d11971af7d
deca <idrisadejumo@gmail.com>
2023-03-14 12:18:04 +0100

    JPA

--- src/main/java/com/adejumo/ReactJS/SpringBoot/CRUD/Application/entity/Employee.java
+++ src/main/java/com/adejumo/ReactJS/SpringBoot/CRUD/Application/entity/Employee.java
@@ -1,2 +1,25 @@
-package com.adejumo.ReactJS.SpringBoot.CRUD.Application.entity;public class Employee {
+package com.adejumo.ReactJS.SpringBoot.CRUD.Application.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@ToString
+@Table(name = "Employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(name = "first_names")
+    private String firstName;
+    @Column(name = "last_names")
+    private String lastName;
+    @Column(name = "email_ids")
+
+    private String emailId;
+
 }
--- src/main/java/com/adejumo/ReactJS/SpringBoot/CRUD/Application/repository/EmployeeRepository.java
+++ src/main/java/com/adejumo/ReactJS/SpringBoot/CRUD/Application/repository/EmployeeRepository.java
@@ -1,2 +1,9 @@
-package com.adejumo.ReactJS.SpringBoot.CRUD.Application.repository;public interface EmployeeRepository {
+package com.adejumo.ReactJS.SpringBoot.CRUD.Application.repository;
+
+import com.adejumo.ReactJS.SpringBoot.CRUD.Application.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
